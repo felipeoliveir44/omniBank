95fa573afdeaf8f8aa02fe1bb6dbc73b47ecddde
felipeoliveir44 <felipeoliveiragg@outlook.com>
2024-03-04 23:37:34 -0300

    feat: Finalizando RF04 - Cadastrar cartão

--- src/main/java/br/com/omnibank/factory/ConnectionFactory.java
+++ src/main/java/br/com/omnibank/factory/ConnectionFactory.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection abrirConexaoBD() {
         try {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/omnibank", "root", "toophotel12");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/omnibank", "root", "root");
 
         } catch(SQLException e) {
             throw new RuntimeException("Erro: " + e.getMessage());
