296d77f96e461332129c6b4b64c247c07e496779
felipeoliveir44 <felipeoliveiragg@outlook.com>
2024-04-03 12:56:52 -0300

    Refactor: Mudança na API para listagem de cartões

--- src/main/java/bank/omnibank/api/dados/cartao/DadosListagemCartao.java
+++ src/main/java/bank/omnibank/api/dados/cartao/DadosListagemCartao.java
@@ -9,11 +9,12 @@ public record DadosListagemCartao(
         Long id,
         String numeroCartao,
         String nomeCliente,
+        String cpfCliente,
         BigDecimal limiteCartao,
         String dataValidade,
         Boolean status) {
     public DadosListagemCartao(Cartao cartao) {
-        this(cartao.getId(), cartao.getNumero(), cartao.getCliente().getNome(), cartao.getLimite(),cartao.getValidade(), cartao.getAtivo());
+        this(cartao.getId(), cartao.getNumero(), cartao.getCliente().getNome(), cartao.getCliente().getCpf(), cartao.getLimite(),cartao.getValidade(), cartao.getAtivo());
     }
 
 }
